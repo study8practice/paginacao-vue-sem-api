32c30d2c27fa7b7a7e8984252fcb2e98578cbe06
study8practice <156129782+study8practice@users.noreply.github.com>
2024-08-08 16:47:49 -0300

    Create ProdutosLista.vue

---
+++ ProdutosLista.vue
@@ -0,0 +1,18 @@
+<template>
+  <ProdutosPaginar />
+</template>
+
+<script>
+import ProdutosPaginar from "@/components/ProdutosPaginar2.vue";
+
+export default {
+  name: "ProdutosLista",
+  components: {
+    ProdutosPaginar
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
